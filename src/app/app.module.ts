357e308e77d2826a591b9be6953b050d05f40432
rahulgore34 <rahulgore34@gmail.com>
2020-03-01 20:17:56 +0530

    addned next 3 pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { HeaderComponent } from "./header/header.component";
 import { Page2Component } from "./page2/page2.component";
 import { Page3Component } from "./page3/page3.component";
 import { Page4Component } from './page4/page4.component';
+import { Page5Component } from './page5/page5.component';
+import { Page6Component } from './page6/page6.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, Page2Component, Page3Component, Page4Component],
+  declarations: [AppComponent, HeaderComponent, Page2Component, Page3Component, Page4Component, Page5Component, Page6Component],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
