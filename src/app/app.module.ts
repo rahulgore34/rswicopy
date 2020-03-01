c410821f3e1ea48afd10464670467e6f9a2105e3
rahulgore34 <rahulgore34@gmail.com>
2020-03-01 14:37:42 +0530

    here next 2 pages is added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { Page2Component } from "./page2/page2.component";
 import { Page3Component } from "./page3/page3.component";
+import { Page4Component } from './page4/page4.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, Page2Component, Page3Component],
+  declarations: [AppComponent, HeaderComponent, Page2Component, Page3Component, Page4Component],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
